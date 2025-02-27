ce1d4d9cbc4b065466c9a2e3b98606e2b5fd8094
Sophie Jean <363449299@GoTVDSB.ca>
2025-02-27 14:03:36 -0500

    Add project files.

---
+++ Topic 5 - If Statements TUTORIAL/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Topic_5___If_Statements_TUTORIAL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
